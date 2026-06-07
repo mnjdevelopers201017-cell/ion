3440f932ffd0ec3f69f53fdcadbc1cdf48cff1a1
mnjdevelopers201017 <mnjdevelopers201017@gmail.com>
2026-06-07 20:24:08 +0600

    Remove broken static export configuration

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,8 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.mnj.ion',
   appName: 'Ion',
-  webDir: 'public'
+  webDir: 'public',
 };
 
 export default config;
+
