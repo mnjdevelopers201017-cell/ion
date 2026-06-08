f231d1206faed794a15d9afb2200dfa6562c99ff
mnjdevelopers201017 <mnjdevelopers201017@gmail.com>
2026-06-08 21:59:52 +0600

    Use Vercel server mode and Java 22

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,8 +3,10 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.mnj.ion',
   appName: 'Ion',
-  webDir: 'public',
+  server: {
+    url: 'https://ion-8dwz-3y2tam4yt-mnj-s-projects.vercel.app',
+    cleartext: true,
+  },
 };
 
 export default config;
-
